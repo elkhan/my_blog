3ebb2f94dcc5444867265c11285dc6377983e994
Elkhan Mamedov <elkhan.me@gmail.com>
2022-10-10 12:44:53 +0200

    fix: add intro entry temporarily

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,6 +64,12 @@ const config = {
             position: 'left',
             label: 'Algorithms',
           },
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Intro',
+          },
           { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/elkhan/my_blog',
