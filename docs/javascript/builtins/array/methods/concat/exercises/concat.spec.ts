2bf747b623ef614be703cfdc238627d5e2d660eb
Elkhan Mamedov <elkhan.me@gmail.com>
2022-10-12 09:57:23 +0200

    feat: add concat() and links to javascript index.mdx

--- docs/javascript/builtins/array/methods/concat/exercises/concat.spec.ts
+++
@@ -1,8 +0,0 @@
-describe('concat', () => {
-  const array_1 = new Array(5).fill(1)
-  const array_2 = new Array(5).fill(2)
-
-  it('should return an array', () => {
-    expect(array_1.concat(array_2)).toEqual([1, 1, 1, 1, 1, 2, 2, 2, 2, 2])
-  })
-})
